4a31e028853a45928074512cc3addfbf0617bdc1
Kikuchi.M <wildflower.pink0102@gmail.com>
2021-12-04 23:34:37 +0900

    fixup! Fix package

--- lib-native/src/main/cpp/jp_co_atware_ac2021_jni_lib_NativeApi.cpp
+++ lib-native/src/main/cpp/jp_co_atware_ac2021_jni_lib_NativeApi.cpp
@@ -2,10 +2,10 @@
 
 #include <jni.h>
 
-jlong Java_jp_co_atware_ac2021_jni_NativeApi_calcByArray(JNIEnv* env, jobject, jobjectArray data) {
+jlong Java_jp_co_atware_ac2021_jni_lib_NativeApi_calcByArray(JNIEnv* env, jobject, jobjectArray data) {
     return 0;
 }
 
-jlong Java_jp_co_atware_ac2021_jni_NativeApi_calcByStream(JNIEnv* env, jobject, jobject iter) {
+jlong Java_jp_co_atware_ac2021_jni_lib_NativeApi_calcByStream(JNIEnv* env, jobject, jobject iter) {
     return 0;
 }
