2b3ef746e3d229be98a8c3925bf264dc08000fe5
Kikuchi.M <wildflower.pink0102@gmail.com>
2021-12-05 00:14:53 +0900

    Implement calculation of native side

--- lib-native/src/main/cpp/jp_co_atware_ac2021_jni_lib_NativeApi.cpp
+++ lib-native/src/main/cpp/jp_co_atware_ac2021_jni_lib_NativeApi.cpp
@@ -3,7 +3,20 @@
 #include <jni.h>
 
 jlong Java_jp_co_atware_ac2021_jni_lib_NativeApi_calcByArray(JNIEnv* env, jobject, jobjectArray data) {
-    return 0;
+
+    jlong result = 0;
+    auto dataSize = env->GetArrayLength(data);
+
+    for (jsize i = 0; i < dataSize; ++i) {
+        auto d = (jintArray) env->GetObjectArrayElement(data, i);
+        auto ds = env->GetArrayLength(d);
+        auto dPtr = env->GetIntArrayElements(d, JNI_FALSE);
+        for (jsize di = 0; di < ds; ++di) {
+            result += dPtr[di];
+        }
+        env->ReleaseIntArrayElements(d, dPtr, JNI_ABORT);
+    }
+    return result;
 }
 
 jlong Java_jp_co_atware_ac2021_jni_lib_NativeApi_calcByStream(JNIEnv* env, jobject, jobject iter) {
