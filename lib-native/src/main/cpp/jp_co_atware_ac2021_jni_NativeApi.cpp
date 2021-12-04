1096bbbc1f8b0b39eab1099dfad8321f556126fc
Kikuchi.M <wildflower.pink0102@gmail.com>
2021-12-04 21:56:26 +0900

    Add native project

---
+++ lib-native/src/main/cpp/jp_co_atware_ac2021_jni_NativeApi.cpp
@@ -0,0 +1,11 @@
+#include "jp_co_atware_ac2021_jni_NativeApi.h"
+
+#include <jni.h>
+
+jlong Java_jp_co_atware_ac2021_jni_NativeApi_calcByArray(JNIEnv* env, jobject, jobjectArray data) {
+    return 0;
+}
+
+jlong Java_jp_co_atware_ac2021_jni_NativeApi_calcByStream(JNIEnv* env, jobject, jobject iter) {
+    return 0;
+}
