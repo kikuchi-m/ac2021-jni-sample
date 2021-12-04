c3574498c2ee7c25e780635d0cb12faaeb96e058
Kikuchi.M <wildflower.pink0102@gmail.com>
2021-12-04 23:00:46 +0900

    Add executable applications

---
+++ app/perform-array/src/main/java/jp/co/atware/ac2021/jni/app/Application.java
@@ -0,0 +1,11 @@
+package jp.co.atware.ac2021.jni.app;
+
+import jp.co.atware.ac2021.jni.lib.NativeApi;
+
+public class Application {
+
+    public static void main(String... args) {
+        System.out.println("JNI performance by array");
+        new NativeApi();
+    }
+}
